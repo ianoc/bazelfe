d730854cf743fd50aa0b64cb1e8e4d48100a4414
Ian O'Connell <ianoc@ianoc.net>
2021-05-19 19:45:29 -0600

    cp

---
+++ bazelfe-core/src/bazel_runner/auto_test_action/command_line_driver.rs
@@ -0,0 +1,56 @@
+use super::event::{Config, Event, Events};
+use super::{app::App, ui};
+use std::{error::Error, io, time::Duration};
+use termion::{event::Key, input::MouseTerminal, raw::IntoRawMode, screen::AlternateScreen};
+use tui::{backend::TermionBackend, Terminal};
+
+pub fn main() -> Result<(), Box<dyn Error>> {
+    let events = Events::with_config(Config {
+        tick_rate: Duration::from_millis(250),
+        ..Config::default()
+    });
+
+    let stdout = io::stdout().into_raw_mode()?;
+    let stdout = MouseTerminal::from(stdout);
+    let stdout = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    let mut app = App::new("Termion demo", true);
+    loop {
+        terminal.draw(|f| ui::draw(f, &mut app))?;
+
+        match events.next()? {
+            Event::Input(key) => match key {
+                Key::Char(c) => {
+                    app.on_key(c);
+                }
+                Key::Up => {
+                    app.on_up();
+                }
+                Key::Down => {
+                    app.on_down();
+                }
+                Key::Left => {
+                    app.on_left();
+                }
+                Key::Right => {
+                    app.on_right();
+                }
+                Key::BackTab => {
+                    app.on_right();
+                }
+
+                _ => {}
+            },
+            Event::Tick => {
+                app.on_tick();
+            }
+        }
+        if app.should_quit {
+            break;
+        }
+    }
+
+    Ok(())
+}

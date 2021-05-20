40593ae549d9919bddd23d8d07d7012fe569bd8f
Ian O'Connell <ianoc@ianoc.net>
2021-05-19 20:16:13 -0600

    cp

--- bazelfe-core/src/bazel_runner/auto_test_action/ui.rs
+++ bazelfe-core/src/bazel_runner/auto_test_action/ui.rs
@@ -239,78 +239,33 @@ fn draw_second_tab<B>(f: &mut Frame<B>, app: &mut App, area: Rect)
 where
     B: Backend,
 {
-    let chunks = Layout::default()
-        .constraints([Constraint::Percentage(30), Constraint::Percentage(70)].as_ref())
-        .direction(Direction::Horizontal)
-        .split(area);
-    let up_style = Style::default().fg(Color::Green);
-    let failure_style = Style::default()
-        .fg(Color::Red)
-        .add_modifier(Modifier::RAPID_BLINK | Modifier::CROSSED_OUT);
-    let header = ["Server", "Location", "Status"];
-    let rows = app.servers.iter().map(|s| {
-        let style = if s.status == "Up" {
-            up_style
-        } else {
-            failure_style
-        };
-        Row::new(vec![s.name, s.location, s.status]).style(style)
-    });
-    let table = Table::new(rows)
-        .header(
-            Row::new(vec!["Server", "Location", "Status"])
-                .style(Style::default().fg(Color::Yellow))
-                .bottom_margin(1),
-        )
-        .block(Block::default().title("Servers").borders(Borders::ALL))
-        .widths(&[
-            Constraint::Length(15),
-            Constraint::Length(15),
-            Constraint::Length(10),
-        ]);
-    f.render_widget(table, chunks[0]);
-
-    let map = Canvas::default()
-        .block(Block::default().title("World").borders(Borders::ALL))
-        .paint(|ctx| {
-            ctx.draw(&Map {
-                color: Color::White,
-                resolution: MapResolution::High,
-            });
-            ctx.layer();
-            ctx.draw(&Rectangle {
-                x: 0.0,
-                y: 30.0,
-                width: 10.0,
-                height: 10.0,
-                color: Color::Yellow,
-            });
-            for (i, s1) in app.servers.iter().enumerate() {
-                for s2 in &app.servers[i + 1..] {
-                    ctx.draw(&Line {
-                        x1: s1.coords.1,
-                        y1: s1.coords.0,
-                        y2: s2.coords.0,
-                        x2: s2.coords.1,
-                        color: Color::Yellow,
-                    });
-                }
-            }
-            for server in &app.servers {
-                let color = if server.status == "Up" {
-                    Color::Green
-                } else {
-                    Color::Red
-                };
-                ctx.print(server.coords.1, server.coords.0, "X", color);
-            }
+    let action_style = Style::default().fg(Color::Blue);
+    let target_style = Style::default().fg(Color::Yellow);
+    let test_style = Style::default().fg(Color::Magenta);
+    let unknown_style = Style::default().fg(Color::Red);
+    let logs: Vec<ListItem> = app
+        .action_logs
+        .items
+        .iter()
+        .map(|&(level, evt, run_time)| {
+            let s = match level {
+                "ACTION" => action_style,
+                "TARGET" => target_style,
+                "TEST" => test_style,
+                _ => unknown_style,
+            };
+            let content = vec![Spans::from(vec![
+                Span::styled(format!("{:<9}", level), s),
+                Span::raw(evt),
+                Span::raw(format!(" in {:?}", Duration::from_secs(run_time.into()))),
+            ])];
+            ListItem::new(content)
         })
-        .marker(if app.enhanced_graphics {
-            symbols::Marker::Braille
-        } else {
-            symbols::Marker::Dot
-        })
-        .x_bounds([-180.0, 180.0])
-        .y_bounds([-90.0, 90.0]);
-    f.render_widget(map, chunks[1]);
+        .collect();
+    let logs = List::new(logs).block(
+        Block::default()
+            .borders(Borders::ALL)
+            .title("Completion events"),
+    );
+    f.render_stateful_widget(logs, area, &mut app.action_logs.state);
 }
